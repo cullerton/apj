568aec2690fed6d568375044339ce07b1a6fb264
mike cullerton <michaelc@cullerton.com>
2015-03-03 21:13:03 -0700

    initial import

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    return app
+
+app = create_app()
+
+
+@app.route('/')
+def hello_world():
+    return render_template('apj.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
